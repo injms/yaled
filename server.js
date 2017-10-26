0cf47377a16b210f41cf7fd111bba9bf7fefc2ca
Ian James <injms@users.noreply.github.com>
2017-10-26 10:51:18 +0100

    Rough first version. Works.

---
+++ server.js
@@ -0,0 +1,53 @@
+const express  = require('express');
+const app      = express();
+const isURL    = require('is-url');
+const settings = require('./config.json');
+
+app.get('/:timing/*', function (req, response) {
+    let delayTime = Math.floor(req.params.timing);
+
+    if (req.params.timing === 'teapot') {
+        response
+            .status(418)
+            .json({
+                error: 'not a valid time - please use a integer'
+            });
+
+    }
+    else if (isURL(req.params[0]) === false) {
+        response
+            .status(400)
+            .json({
+                error: 'please use a valid URL'
+            });
+
+    }
+    else if (Number.isInteger(delayTime) === true) {
+        if ( delayTime > settings.maxDelay ) {
+            response
+                .status(400)
+                .json({
+                    error: `time too long - must be shorter than ${settings.maxDelay} seconds`
+                });
+
+        }
+        else {
+            setTimeout( () => {
+                    response.redirect(307, req.params[0])
+                }, delayTime * 1000);
+
+        }
+    }
+    else {
+        response
+            .status(400)
+            .json({
+                error: 'not a valid time - please use a integer'
+            });
+
+    }
+})
+
+app.listen(3000, function () {
+    console.log('Yaled listening on port 3000!')
+})
